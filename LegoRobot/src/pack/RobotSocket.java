4865fe8bdb30155e3c47901945dd3bdcf87dfcdb
franekwoz <franek2003@web.de>
2018-05-13 17:08:05 +0200

    RobotSocket Klasse

---
+++ LegoRobot/src/pack/RobotSocket.java
@@ -0,0 +1,185 @@
+package pack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import lejos.utility.Delay;
+
+
+
+public class RobotSocket extends Thread {
+	
+	private Socket socket;
+	private InputStream is;
+	private OutputStream os;
+	private ServerSocket ss;
+	private PrintWriter out=null;
+	private String inMessage="";
+	private boolean isMessage=false;
+	private String outMessage="";
+	
+
+	RobotSocket(int port)
+	{
+		
+		try {
+			ss = new ServerSocket(port);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public  synchronized String getMessage() throws InterruptedException
+	{
+		while (!isMessage)
+			wait();
+		isMessage = false;
+		return inMessage;
+		
+	}
+	
+	public  synchronized String getMessage(boolean immediateReturn) throws InterruptedException
+	{
+		if (immediateReturn)
+			return getMessageNonBlocking();
+		else 
+			return getMessage();
+	}
+	
+	public  synchronized void sendMessage(String outmessage)
+	{
+		out.println(outmessage);
+	}
+	
+	public  synchronized void sendMessage(String outmessage, boolean noError)
+	{
+		if (noError)
+			outMessage=outmessage;
+		else
+			sendMessage(outmessage);
+	}
+	
+	
+	
+	private  synchronized String getMessageNonBlocking() throws InterruptedException
+	{
+		if (isMessage)
+		{
+			isMessage = false;
+			 return inMessage;
+			
+		}
+		else 
+			return null;
+		
+	}
+	
+		
+	private synchronized void setInMessage(String inmessage) {
+		inMessage = inmessage;
+		isMessage=true;
+		notify();
+	}
+	
+	
+	
+	
+	private synchronized String getOutMessage() {
+		String copyOutMessage = outMessage;
+		outMessage="";
+		return copyOutMessage;
+	}
+	
+	
+	
+	
+	
+	
+
+	public  void  run() 
+	{     
+		String localOutMessage="";
+		String localInMessage="";
+    		
+    		try {
+    		
+    			
+    			ss.getInetAddress();
+				System.out.println("Wait for Connection: " + InetAddress.getLocalHost().getHostAddress());
+    			socket = ss.accept();
+    			
+    			is = socket.getInputStream();
+    			os = socket.getOutputStream();
+    			System.out.println("Conected!!!");
+    			
+    			out = new PrintWriter(os, true);
+    			BufferedReader in = new BufferedReader(new InputStreamReader(is));
+    			
+	    		
+	    		
+	    		while (true)
+	    		{
+	    			
+	        	
+	        	
+	        		System.out.println("Wait for Message...");
+	        		localInMessage  = in.readLine();
+	            	
+	            	
+	            	if (localInMessage==null)
+	            	{
+	            		
+	            		System.out.println("Null received");
+	            		socket.close();
+	        			ss.close();
+	            		break;
+	            	}else {
+	            	
+	            		setInMessage(localInMessage);
+	            		
+	            	}
+	            	
+	            	localOutMessage=getOutMessage();
+	            	if (localOutMessage!="")
+	            	{
+	            		out.println(localOutMessage);
+	            		
+	            	}
+	            		
+	            	
+	            		            	
+	            	
+	            	
+	            	
+	            	
+	            	Delay.msDelay(100);
+	    		}
+	    		
+	    		
+	    	} 
+        	catch (IOException e) 
+        	{
+        		e.printStackTrace(System.out);
+        	}
+    		finally
+    		{
+    			try {
+    				socket.close();
+					ss.close();
+				} catch (IOException e) {
+					e.printStackTrace(System.out);
+				}
+    		}
+    		
+    		System.out.println("End of RobotSocket Thread!");
+    		
+	}
+}
